30ae123969cd41948e97dc9c06ef8b22cbb63820
denismoscvin <moscvindenis95@gmail.com>
2023-08-18 11:14:01 +0300

    first real commit

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,26 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("User is in NetBanking Landing page")
+    public void userIsInNetBankingLandingPage() {
+        System.out.println("a");
+    }
+
+    @When("User login into application with username and password")
+    public void userLoginIntoApplicationWithUsernameAndPassword() {
+        System.out.println("b");
+    }
+
+    @Then("Home page is populated")
+    public void homePageIsPopulated() {
+        System.out.println("c");
+    }
+
+    @And("Cards are displayed")
+    public void cardsAreDisplayed() {
+        System.out.println("d");
+    }
+}
