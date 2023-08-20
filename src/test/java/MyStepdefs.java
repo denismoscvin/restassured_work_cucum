91f225269c6ffaab226b6a8cf273ef2f1741c85b
denismoscvin <moscvindenis95@gmail.com>
2023-08-20 14:01:55 +0300

    modified println

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 public class MyStepdefs {
     @Given("User is in NetBanking Landing page")
     public void userIsInNetBankingLandingPage() {
-        System.out.println("a");
+        System.out.println("1) Enter login URL!");
     }
 
     @When("User login into application with login {string} and password {string}")
@@ -19,12 +19,12 @@ public class MyStepdefs {
 
     @Then("Home page is populated")
     public void homePageIsPopulated() {
-        System.out.println("c");
+        System.out.println("3) Validate the form!");
     }
 
     @And("Cards are displayed")
     public void cardsAreDisplayed() {
-        System.out.println("d");
+        System.out.println("4) Displaying cards");
     }
 
 }
