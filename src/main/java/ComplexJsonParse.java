57e295fec5383b03b59291ab57db933da4a875db
denismoscvin <moscvindenis95@gmail.com>
2023-08-28 14:05:05 +0300

    update 1

--- src/main/java/ComplexJsonParse.java
+++ src/main/java/ComplexJsonParse.java
@@ -14,16 +14,30 @@ public class ComplexJsonParse {
         System.out.println(summ);
 
         //Display title of first course
+        System.out.println();
+        System.out.println("-----Display title of first course-----");
         String zaibal = jsonPath.getString("courses[0].title");
         System.out.println(zaibal);
 
         //Display all courses
+        System.out.println();
+        System.out.println("-----Display all courses-----");
         for (int i = 0; i < count; i++) {
             String coursesTitle = jsonPath.getJsonObject("courses[" + i + "].title");
             System.out.println(coursesTitle);
         }
 
         //Display price for RPA course
+        System.out.println();
+        System.out.println("-----Display price for RPA course-----");
+        for (int i = 0; i < count; i++) {
+            String coursesTitle = jsonPath.get("courses[" + i + "].title");
+            if (coursesTitle.equalsIgnoreCase("RPA")) {
+                int copies = jsonPath.get("courses[" + i + "].copies");
+                System.out.println(copies);
+                break;
+            }
+        }
 
     }
 }
---
+++ src/main/java/SumValidation.java
@@ -0,0 +1,26 @@
+import files.payload;
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.Test;
+
+public class SumValidation {
+
+    @Test
+    public void sumOfCourses() {
+        JsonPath jsonPath = new JsonPath(payload.CoursePrice());
+        int count = jsonPath.getInt("courses.size()");
+        System.out.println("We have " + count + " courses:");
+        int sum = 0;
+
+        for (int i = 0; i < count; i++) {
+            int price = jsonPath.get("courses[" + i + "].price");
+            int copies = jsonPath.get("courses[" + i + "].copies");
+            String title = jsonPath.get("courses[" + i + "].title");
+            int amount = price * copies;
+            System.out.println(i + ") " + "For " + title + " must be pay ->" + " " + amount);
+
+            sum = sum + amount;
+        }
+        System.out.println("Summ for all courses is " + sum);
+    }
+
+}
